2b4b16956ef92050b0b6d6f48ca5ec2108664373
Kurt Schoenhoff <Kurt@avlx.com.au>
2015-07-31 15:52:00 +1000

    first real commit

---
+++ addon.py
@@ -0,0 +1,20 @@
+﻿import xbmcaddon
+import xbmcgui
+import subprocess
+ 
+addon       = xbmcaddon.Addon()
+addonname   = addon.getAddonInfo('name')
+ 
+line1 = "Hello World!"
+line2 = "We will now destroy chrome"
+line3 = "Using Python to run a command in shell"
+ 
+xbmcgui.Dialog().ok(addonname, line1, line2, line3)
+
+
+log('Killing all chrome tabs')
+
+try:
+	subprocess.call(['xdotool search --name chrome windowkill',shell=True])
+except:
+    log('ERROR executing script killChrome')
